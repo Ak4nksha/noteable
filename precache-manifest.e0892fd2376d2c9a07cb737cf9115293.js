b4ca21a0323b780dbc121093458acd19ea0dc8ae
Ak4nksha <aakankshaa2606@gmail.com>
2019-03-06 19:47:57 +0530

    Updates

---
+++ precache-manifest.e0892fd2376d2c9a07cb737cf9115293.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70d311c088a70b0b339",
+    "url": "/noteable/static/css/main.4a2b70d3.chunk.css"
+  },
+  {
+    "revision": "a70d311c088a70b0b339",
+    "url": "/noteable/static/js/main.a70d311c.chunk.js"
+  },
+  {
+    "revision": "20bc491bae5e667480bb",
+    "url": "/noteable/static/js/1.20bc491b.chunk.js"
+  },
+  {
+    "revision": "32bae516fefda970ddc8",
+    "url": "/noteable/static/js/runtime~main.32bae516.js"
+  },
+  {
+    "revision": "f984d40964c29eb94f9e544828dce837",
+    "url": "/noteable/static/media/bg.f984d409.jpg"
+  },
+  {
+    "revision": "ba49af1784245752b67e05726556fe6c",
+    "url": "/noteable/index.html"
+  }
+];
